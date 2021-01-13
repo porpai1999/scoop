62ea45e1827b1e9a9e77233225ee87244093b2c5
porpai1999 <porpai1999@gmail.com>
2021-01-13 07:51:24 +0700

    fixed back end

--- node/dbconnection.js
+++ node/dbconnection.js
@@ -1,18 +1,10 @@
 const mysql      = require('mysql');
-// const connection = mysql.createPool({
-//     connectionLimit : 10,
-//     // host     : '27.254.172.42:3306',
-//     host     : 'localhost:3306',
-//     user     : 'scoop',
-//     password : 'a33^d7Rg',
-//     database : 'comsci_scoop'
-// });
 
 const connection = mysql.createPool({
     connectionLimit : 10,
     host     : 'localhost',
-    user     : 'root',
-    password : '',
+    user     : 'scoop',
+    password : 'a33^d7Rg',
     database : 'comsci_scoop'
 });
 
