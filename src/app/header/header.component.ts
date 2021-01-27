c567ba6e591f6c06bf99c2840c6cdab4bfe274ad
lenovo <lenovo@mail.com>
2021-01-27 22:48:05 +0700

    up petch

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatapassService } from '../datapass.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  emails;
+  constructor(private router : Router, private data : DatapassService, private acRouter : ActivatedRoute,
+    private http: HttpClient) { 
+      
+      let email = acRouter.snapshot.params['p1'];
+      this.emails = email;
+      console.log('p1--->',this.emails);
+      
+    }
 
   ngOnInit(): void {
-  
+    // this.http.get('http://localhost:3000/users/select_some/'+this.emails)
+    //    .subscribe(data => {
+    //     console.log(data);
+    //   },error =>{
+    //     console.log(error);
+    //   });
   }
 
 }
