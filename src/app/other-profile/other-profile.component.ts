99233c70b78af31138ea4816eeb12be80ce70d47
porpai1999 <porpai1999@gmail.com>
2021-03-19 10:45:04 +0700

    other-profile

--- src/app/other-profile/other-profile.component.ts
+++ src/app/other-profile/other-profile.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DatapassService } from '../datapass.service';
 
 @Component({
   selector: 'app-other-profile',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherProfileComponent implements OnInit {
 
-  constructor() { }
+  fullname;
+  id;
+  imgpath;
+  myID
+  followers;
+  following;
+
+  constructor(private router : Router, private data : DatapassService, private acRouter : ActivatedRoute,
+    private http: HttpClient) { 
+      let ids = acRouter.snapshot.params['p1'];
+      this.id = ids;
+      this.myID = sessionStorage.getItem("keyuser_id");
+      this.imgpath = "http://nodescoop.comsciproject.com/images/jpfunnyface.png"
+      console.log(ids);
+    }
 
   ngOnInit(): void {
-  }
-
+    this.http.get('http://localhost:3000/profiler/profile/'+this.id)
+       .subscribe(response => {
+        this.fullname = response[0].first_name +' '+response[0].last_name;
+        console.log('fullname :',this.fullname);
+        this.http.get('http://localhost:3000/profiler/show_followers_c/'+this.id).subscribe(response => {
+          let items = [];
+          for (let key in response) {
+            if (response.hasOwnProperty(key)) {
+              items.push(response[key]);
+            }
+          }
+          this.followers = items[0][0].followers
+          this.http.get('http://localhost:3000/profiler/show_following_c/'+this.id).subscribe(response => {
+          let items = [];
+            for (let key in response) {
+              if (response.hasOwnProperty(key)) {
+                items.push(response[key]);
+              }
+            }
+            this.following = items[0][0].following
+        });
+        });
+      },error =>{
+        console.log(error);
+      });
+      }
 }
