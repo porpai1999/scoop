e7b7457bd642ce396f4d21beb60f8c5c19eea679
scoop <jjagy300@gmail.com>
2021-04-01 05:11:01 +0700

    Alet Register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { clear } from 'console';
 import { DatapassService } from '../datapass.service';
 
 
@@ -27,6 +28,14 @@ export class RegisterComponent implements OnInit {
   base64;
   url;
 
+  displayPosition: boolean = false;
+  position: string = "";
+  regisstatus:number=0;
+  displayPosition1: boolean = false;
+  displayPosition2: boolean = false;
+  namefeild = [];
+  
+
   //sanitizer;
   file_img:any;
   file :any;
@@ -42,8 +51,53 @@ export class RegisterComponent implements OnInit {
     this.siteKey = "6LebNC0aAAAAAOC8dWexryo1xwYyLCy8G3Ipa7a7";
     this.recaptcha= false;
   }
-
-  register() {
+  click(){
+    if (this.regisstatus == 1) {
+      this.displayPosition = false;
+      this.router.navigateByUrl('/login');
+    }
+    else if (this.regisstatus == 0){
+      this.displayPosition2 = false;
+      this.namefeild = [];
+    } 
+    else  if (this.regisstatus == 2){
+      this.displayPosition1 = false;
+    }
+  }
+  checkvalue(position: string){
+    if(this.first_name == undefined){
+      this.namefeild.push("--> Name Invalid");
+    }
+    if (this.last_name == undefined) {
+      this.namefeild.push("--> Surname Invalid");
+    }
+    if (this.email == undefined) {
+      this.namefeild.push("--> Email Invalid");
+    }
+    if(this.password == undefined){
+      this.namefeild.push("--> Password Invalid");
+    }
+    if(this.birthDay == undefined){
+      this.namefeild.push("--> Birthday Invalid");
+    }
+    if (this.gender == undefined) {
+      this.namefeild.push("--> Gender Invalid");
+    }
+    if (this.first_name != undefined && this.last_name != undefined && this.email != undefined && this.password != undefined && this.birthDay != undefined && this.gender != undefined) {
+      this.register('top');
+    }
+    if(this.namefeild.length > 0){
+      this.regisstatus = 0;
+      this.position = position;
+      this.displayPosition2 = true;
+      console.log("failed");
+    }
+    else{
+      console.log("++++");
+    }
+    console.log(this.namefeild);
+  }
+  register(position: string) {
     let date = new Date();
     let currentDate = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
     let items = [];
@@ -52,19 +106,26 @@ export class RegisterComponent implements OnInit {
       let register_json = { photo_id: 0, email: this.email, password: this.password, first_name: this.first_name, last_name: this.last_name, date_of_birth: date_of_birth, gender: this.gender };
       this.http.post(this.host+'/auth/register', register_json).subscribe(response => {
         if (response) {
+          console.log(response);
           let jsonObj: any = response;
+          console.log(jsonObj);
           this.url = jsonObj.url;
 
           for (let key in response) {
             if (response.hasOwnProperty(key)) {
               items.push(response[key]);
+              console.log(response[key]);
+              
             }
           }
+          console.log(items);
           let registered_userID = items[1];
           console.log("1"+registered_userID);
+          console.log(this.file);
 
           if (this.file == undefined) {
             this.router.navigateByUrl('/login');
+            console.log(this.file);
           } else {
             let formData: any = new FormData();
             formData.append("file", this.file);
@@ -112,7 +173,11 @@ export class RegisterComponent implements OnInit {
                             
                             this.http.post(this.host+'/users/profile_photo/'+registered_userID, profile_photo_json).subscribe(response => {
                               if (response) {
-                                this.router.navigateByUrl('/login');
+                                this.position = position;
+                                this.displayPosition = true;
+                                console.log("Success");
+                                this.regisstatus = 1;
+                                // this.router.navigateByUrl('/login');
                               } else {
                                 console.log('Status : insert_failed');
                               }
@@ -154,10 +219,13 @@ export class RegisterComponent implements OnInit {
       else{
         sessionStorage.getItem("result");
       }
-    } else {
+    } else{
       console.log(`reCaptcha : ${this.recaptcha}`);
+      this.regisstatus = 2;
+      this.position = position;
+      this.displayPosition1 = true;
+      console.log("failed");
     }
-    
   }
 
   ngOnInit(): void {
