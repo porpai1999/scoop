d171cb8941f5a958be2ca628dd02e8760ee62b5c
porpai1999 <porpai1999@gmail.com>
2021-04-01 22:23:13 +0700

    update

--- src/app/datapass.service.ts
+++ src/app/datapass.service.ts
@@ -7,7 +7,7 @@ export class DatapassService {
   account;
   // user_img = "http://nodescoop.comsciproject.com/images/userprofile.png";
   // other_user_img = "http://nodescoop.comsciproject.com/images/userprofile.png";
-  // host = "http://nodescoop.comsciproject.com";
-  host = "http://localhost:3000";
+  host = "http://nodescoop.comsciproject.com";
+  // host = "http://localhost:3000";
   constructor() { }
 }
